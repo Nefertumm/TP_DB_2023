4721da13ed8bc7366ab8de991678b967fe6e49d7
Nefertumm <nefertum.dev@protonmail.com>
2023-06-06 17:13:28 -0300

    fix constraint + typo

--- DDL.sql
+++ DDL.sql
@@ -1,5 +1,5 @@
 CREATE TABLE employee(
-    employee_id INT NOT NULL PRIMARY KEY ,
+    employee_id INT NOT NULL PRIMARY KEY,
     last_name VARCHAR(20) NOT NULL,
     first_name VARCHAR(20) NOT NULL,
     title VARCHAR(30),
@@ -15,6 +15,7 @@ CREATE TABLE employee(
     fax VARCHAR(24),
     email VARCHAR(60)
 );
+
 CREATE TABLE customer(
     customer_id INT NOT NULL PRIMARY KEY,
     first_name VARCHAR(40) NOT NULL,
@@ -30,6 +31,7 @@ CREATE TABLE customer(
     email VARCHAR(60),
     support_rep_id INT
 );
+
 CREATE TABLE invoice(
     invoice_id INT NOT NULL PRIMARY KEY,
     customer_id INT NOT NULL,
@@ -48,6 +50,7 @@ CREATE TABLE invoice_line(
     unit_price DECIMAL(10,2) NOT NULL,
     quantity INT NOT NULL
 );
+
 CREATE TABLE track(
     track_id INT NOT NULL PRIMARY KEY,
     name VARCHAR(200) NOT NULL,
@@ -59,48 +62,53 @@ CREATE TABLE track(
     bytes INT,
     unit_price DECIMAL(10,2) NOT NULL
 );
+
 CREATE TABLE album(
     album_id INT NOT NULL PRIMARY KEY,
     title VARCHAR(160) NOT NULL,
     artist_id INT NOT NULL
 );
+
 CREATE TABLE artist(
     artist_id INT NOT NULL PRIMARY KEY,
     name VARCHAR(120)
 );
+
 CREATE TABLE genre(
     genre_id INT NOT NULL PRIMARY KEY,
     name VARCHAR(120)
 );
+
 CREATE TABLE media_type(
     media_type_id INT NOT NULL PRIMARY KEY,
     name VARCHAR(120)
 );
+
 CREATE TABLE playlist(
     playlist_id INT NOT NULL PRIMARY KEY,
     name VARCHAR(120)
 );
+
 CREATE TABLE playlist_track(
-    playlist_id INT NOT NULL
-        CONSTRAINT "FK_playlist_track_playlist_id" REFERENCES "playlist" ,
-    track_id INT NOT NULL,
-        CONSTRAINT "FK_playlist_track_track_id" REFERENCES "track",
-    CONSTRAINT "PK_playlist_track" 
-        PRIMARY KEY (playlist_id, track_id)
+    playlist_id INT NOT NULL,
+    track_id INT NOT NULL
 );
 
 ALTER TABLE employee
     ADD CONSTRAINT "FK_employee_reports_to"
-        FOREIGN KEY (report_to)
+        FOREIGN KEY (reports_to)
         REFERENCES employee(employee_id);
+
 ALTER TABLE customer
     ADD CONSTRAINT "FK_customer_support_rep_id"
         FOREIGN KEY (support_rep_id)
         REFERENCES employee(employee_id);
+
 ALTER TABLE invoice
     ADD CONSTRAINT "FK_invoice_customer_id"
         FOREIGN KEY (customer_id)
         REFERENCES customer(customer_id);
+
 ALTER TABLE invoice_line
     ADD CONSTRAINT "FK_invoice_line_invoice_id"
         FOREIGN KEY (invoice_id)
@@ -108,6 +116,7 @@ ALTER TABLE invoice_line
     ADD CONSTRAINT "FK_invoice_line_track_id"
         FOREIGN KEY (track_id)
         REFERENCES track(track_id);
+
 ALTER TABLE track
     ADD CONSTRAINT "FK_track_album_id"
         FOREIGN KEY (album_id)
@@ -118,10 +127,12 @@ ALTER TABLE track
     ADD CONSTRAINT "FK_track_media_type_id"
         FOREIGN KEY (media_type_id)
         REFERENCES media_type(media_type_id);
+
 ALTER TABLE album
     ADD CONSTRAINT "FK_album_artist_id"
         FOREIGN KEY (artist_id)
         REFERENCES artist(artist_id);
+
 ALTER TABLE playlist_track
     ADD CONSTRAINT "FK_playlist_track_playlist_id"
         FOREIGN KEY (playlist_id)
@@ -129,4 +140,3 @@ ALTER TABLE playlist_track
     ADD CONSTRAINT "FK_playlist_track_track_id"
         FOREIGN KEY (track_id)
         REFERENCES track(track_id);
-```
